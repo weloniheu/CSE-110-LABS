67d2a5f8596b0865827e5525ea7933b570309f2b
Welokiheiakea'eloa Gosline-Niheu <weloniheu@gmail.com>
2024-10-07 15:36:32 -0700

    myapp moved

---
+++ LAB2/my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+ return (
+   <div className='app-container'>
+     <div className="note-individual">
+       <button>
+         x
+       </button>
+       <h2> 1st Note Title </h2>
+       <p> 1st Note Content </p>
+       <p> 1st Note Label </p>
+     </div>
+   </div>
+
+ );
+}
+
+export default App;
