5075025426dff6b5449d9f02689b956b23dbc078
weloniheu <weloniheu@gmail.com>
2024-11-04 21:40:28 -0800

    updateBudget in server/budget/budget-utils.ts

--- LAB5/server/src/budget/budget-utils.ts
+++ LAB5/server/src/budget/budget-utils.ts
@@ -7,5 +7,14 @@ export function getBudget(res: Response, budget: number) {
 
 // Function to update the budget
 export function updateBudget(res: Response, body: any, budget: { amount: number }) {
-    // TO DO: Implement updateBudget function
+    const { newBudget } = body;
+
+    if (typeof newBudget !== "number" || newBudget < 0){
+        return res.status(400).send({ error: "Invalid budget amount. Must but a number great that 0"});
+    
+    }
+
+    budget.amount = newBudget;
+
+    res.status(200).send({ message: "Budget udpate successfully", data: budget})
 }
